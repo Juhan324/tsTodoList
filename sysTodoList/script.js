dceec41f5e856388c343af85a21718b50c799f2b
Juhan324 <wngks0820@gmail.com>
2022-08-15 03:44:04 +0900

    1. detail 변경 2. 로컬 스토리지에 데이터 저장 3. 공백 입력 시 알림 출력

--- sysTodoList/script.js
+++ sysTodoList/script.js
@@ -1,13 +1,32 @@
 function insertTodo(){
     let insert = document.getElementById("insert").value;
+    if(insert.trim(" ") == ""){
+        alert("일정을 입력해주세요.");
+        return false;
+    }
+    localStorage.setItem(localStorage.length, insert);
+    initialize(insert);
+    document.getElementById("insert").value="";
+};
+
+function readTodo() {
+    for (let index = 0; index < localStorage.length; index++) {
+        let item = "";
+        item = localStorage.getItem(index);
+        if(item != null){
+            initialize(item);
+        }
+    }
+}
+
+function initialize(item) {
     let tr = document.createElement("tr");
     let tdCheck = document.createElement("td");
     let td = document.createElement("td");
     let checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-    checkbox.id = insert;
-    td.id = insert;
-    
+    checkbox.id = item;
+    td.id = item;
     checkbox.onchange = function() {
         if(checkbox.checked){
             td.style.textDecorationLine="line-through";
@@ -16,12 +35,17 @@ function insertTodo(){
             td.style.textDecorationLine="none";
             td.style.color="black";
         }
+        localStorage.setItem(checkbox.id, checkbox.checked);
     };
-
-    td.appendChild(document.createTextNode(insert));
+    
+    td.appendChild(document.createTextNode(item));
     tdCheck.appendChild(checkbox);
     tr.appendChild(tdCheck);
     tr.appendChild(td);
     document.getElementById("todoList").after(tr);
-    document.getElementById("insert").value="";
-};
+    if(localStorage.getItem(checkbox.id)=="true") {
+        checkbox.checked = true;
+        td.style.textDecorationLine="line-through";
+        td.style.color="grey";
+    }
+}
