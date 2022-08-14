da6a13f98ca6db41fd78d851a780f5220fa38310
Juhan324 <wngks0820@gmail.com>
2022-08-14 22:06:01 +0900

    체크 박스 선택 시 취소선

--- sysTodoList/script.js
+++ sysTodoList/script.js
@@ -5,13 +5,23 @@ function insertTodo(){
     let td = document.createElement("td");
     let checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-    checkbox.value = insert;
+    checkbox.id = insert;
+    td.id = insert;
+    
+    checkbox.onchange = function() {
+        if(checkbox.checked){
+            td.style.textDecorationLine="line-through";
+            td.style.color="grey";
+        }else {
+            td.style.textDecorationLine="none";
+            td.style.color="black";
+        }
+    };
+
     td.appendChild(document.createTextNode(insert));
     tdCheck.appendChild(checkbox);
     tr.appendChild(tdCheck);
     tr.appendChild(td);
     document.getElementById("todoList").after(tr);
+    document.getElementById("insert").value="";
 };
-
-function complete(){
-}
